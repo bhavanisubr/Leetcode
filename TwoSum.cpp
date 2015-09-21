7bdd7c659a3f6f900de4e00c332dcaa44dceb069
Bhavani Subramanian <bhasubr@gmail.com>
2015-09-21 14:40:33 -0700

    Update TwoSum.cpp

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> outputIndices;
-		map<int, int> numsMap;
-		for (int i = 0; i < nums.size(); i++)
-			numsMap[nums[i]] = i;
-		for (int i = 0; i < nums.size(); i++)
+	map<int, int> numsMap;
+	for (int i = 0; i < nums.size(); i++)
+		numsMap[nums[i]] = i;
+	for (int i = 0; i < nums.size(); i++)
+	{
+		int curValComplement = target - nums[i];
+		auto it = numsMap.find(curValComplement);
+		if (it != numsMap.end() && it->second != i)	
 		{
-			int curValComplement = target - nums[i];
-			auto it = numsMap.find(curValComplement);
-			if (it != numsMap.end() && it->second != i)	
-			{
-				outputIndices.push_back(i + 1);
-				outputIndices.push_back(it->second + 1);
-				return outputIndices;
-			}
+			outputIndices.push_back(i + 1);
+			outputIndices.push_back(it->second + 1);
+			return outputIndices;
 		}
-		return outputIndices;
+	}
+	return outputIndices;
     }
 };
