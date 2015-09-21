22ceeb99d7c7b7061ac2d6e3ed52067dfc6c6cc7
Bhavani Subramanian <bhasubr@gmail.com>
2015-09-21 15:44:35 -0700

    Merge Two Sorted Lists

--- MergeTwoSortedLists.cpp
+++ MergeTwoSortedLists.cpp
@@ -9,33 +9,29 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if (!l1 && l2)  return l2;
-        if (l1 && !l2)  return l1;
-        if (!l1 && !l2) return NULL;
-        ListNode* head = NULL, *tail = NULL;
-        while (l1 && l2)
-        {
-            ListNode* minNode;
-            if (l1->val <= l2->val)
-            {
-                minNode = l1;
-                l1 = l1->next;
-            }
-            else
-            {
-                minNode = l2;
-                l2 = l2->next;
-            }
-            if (!tail)
-            {
-                tail = minNode;
-                head = minNode;
-                continue;
-            }
-            tail->next = minNode;
-            tail = tail->next;
-        }
-        tail->next = l1 ? l1 : l2;
-        return head;
+       if (!l1 && l2)   return l2;
+       if (l1 && !l2)   return l1;
+       if (!l1 && !l2)  return NULL;
+       ListNode* head = NULL, *tail = NULL;
+       while (l1 && l2) {
+          ListNode* minNode;
+          if (l1->val <= l2->val) {
+             minNode = l1;
+             l1 = l1->next;
+          }
+          else {
+             minNode = l2;
+             l2 = l2->next;
+          }
+          if (!tail) {
+             tail = minNode;
+             head = minNode;
+             continue;
+          }
+          tail->next = minNode;
+          tail = tail->next;
+       }
+       tail->next = l1 ? l1 : l2;
+       return head;
     }
 };
