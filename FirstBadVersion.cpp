88712d1d8e516962f548186c0183f3692fa698f4
bhavanisubr <bhasubr@gmail.com>
2015-09-21 13:55:18 -0700

    First Bad Version

---
+++ FirstBadVersion.cpp
@@ -0,0 +1,16 @@
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int begin = 1, end = n;
+        while (begin <= end)
+        {
+            if (begin == end && isBadVersion(begin))    return begin;
+            int mid = begin + (end - begin) / 2;
+            if (isBadVersion(mid))  end = mid - 1;
+            else    begin = mid + 1;
+        }
+    }
+};
