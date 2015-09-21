d0babacdb60956d5323e9ca465cd67b667a6910b
bhavanisubr <bhasubr@gmail.com>
2015-09-21 13:48:14 -0700

    Climbing Stairs

---
+++ ClimbingStairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        map<int, int> numWaysMap;
+		numWaysMap[0] = 1;
+		numWaysMap[1] = 1;
+		for (int i = 2; i <= n; i++)
+			numWaysMap[i] = numWaysMap[i - 1] + numWaysMap[i - 2];
+		return numWaysMap[n];
+    }
+};
