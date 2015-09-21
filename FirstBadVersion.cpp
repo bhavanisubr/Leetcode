90e8252265025c69ef020ee433aa116078dd07b1
Bhavani Subramanian <bhasubr@gmail.com>
2015-09-21 15:30:39 -0700

    First Bad Version

--- FirstBadVersion.cpp
+++ FirstBadVersion.cpp
@@ -4,13 +4,12 @@ bool isBadVersion(int version);
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int begin = 1, end = n;
-        while (begin <= end)
-        {
-            if (begin == end && isBadVersion(begin))    return begin;
-            int mid = begin + (end - begin) / 2;
-            if (isBadVersion(mid))  end = mid - 1;
-            else    begin = mid + 1;
-        }
+       int begin = 1, end = n;
+       while (begin <= end) {
+          if (begin == end && isBadVersion(begin))   return begin;
+          int mid = begin + (end - begin) / 2;
+          if (isBadVersion(mid))   end = mid - 1;
+          else   begin = mid + 1;
+       }
     }
 };
