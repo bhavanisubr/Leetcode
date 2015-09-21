de97a9e459dffcb5fb0e4ebfc0e5aec9f4051c2e
bhavanisubr <bhasubr@gmail.com>
2015-09-21 13:54:04 -0700

    Factorial Trailing Zeroes

---
+++ FactorialTrailingZeros.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int count = 0;
+        for (long factor = 5; n / factor > 0; factor *= 5)
+            count += n / factor;
+        return count;
+    }
+};
