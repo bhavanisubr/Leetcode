b850665f8dc0c31949a8d05ad025f0f61b0734d6
Bhavani Subramanian <bhasubr@gmail.com>
2015-09-21 15:01:06 -0700

    Climbing Stairs

--- ClimbingStairs.cpp
+++ ClimbingStairs.cpp
@@ -5,7 +5,7 @@ public:
 	numWaysMap[0] = 1;
 	numWaysMap[1] = 1;
 	for (int i = 2; i <= n; i++)
-	    numWaysMap[i] = numWaysMap[i - 1] + numWaysMap[i - 2];
+	   numWaysMap[i] = numWaysMap[i - 1] + numWaysMap[i - 2];
 	return numWaysMap[n];
     }
 };
