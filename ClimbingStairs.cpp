b3c55d53de3f35515f42b8e00af29a3b17cd9619
Bhavani Subramanian <bhasubr@gmail.com>
2015-09-21 15:09:21 -0700

    Climbing Stairs

--- ClimbingStairs.cpp
+++ ClimbingStairs.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int climbStairs(int n) {
         map<int, int> numWaysMap;
-	numWaysMap[0] = 1;
-	numWaysMap[1] = 1;
-	for (int i = 2; i <= n; i++)
-	   numWaysMap[i] = numWaysMap[i - 1] + numWaysMap[i - 2];
-	return numWaysMap[n];
+	    numWaysMap[0] = 1;
+	    numWaysMap[1] = 1;
+	    for (int i = 2; i <= n; i++)
+	        numWaysMap[i] = numWaysMap[i - 1] + numWaysMap[i - 2];
+	    return numWaysMap[n];
     }
 };
