3f93667358a297608b2e0f494af11869bed143bd
Bhavani Subramanian <bhasubr@gmail.com>
2015-09-21 15:32:17 -0700

    Happy Number

--- HappyNumber.cpp
+++ HappyNumber.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        if (n <= 0) return false;
-	int result = 0;
-	while (n)
-	{
-		int rem = n % 10;
-		result += rem * rem;
-		n /= 10;
-	}
-	if (result == 1)	return true;
-	if (result < 10)    	return false;
-	return isHappy(result);
+       if (n <= 0)   return false;
+       int result = 0;
+       while (n) {
+          int rem = n % 10;
+	  result += rem * rem;
+	  n /= 10;
+       }
+       if (result == 1)   return true;
+       if (result < 10)   return false;
+       return isHappy(result);
     }
 };
