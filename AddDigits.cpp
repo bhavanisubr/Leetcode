94441593755e2daae0435df911c84f12dbae71a5
Bhavani Subramanian <bhasubr@gmail.com>
2015-09-21 15:08:07 -0700

    Add Digits

--- AddDigits.cpp
+++ AddDigits.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int addDigits(int num) {
         // Trivial solution:
-        /* int sum = 0;
+        /*  int sum = 0;
 	    while (num) {
 	        sum += num % 10;
 	        num /= 10;
