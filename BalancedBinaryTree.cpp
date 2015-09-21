b980da2ebd7cddf2cdde5993fa9c50e839f88c3c
Bhavani Subramanian <bhasubr@gmail.com>
2015-09-21 14:57:51 -0700

    Balanced Binary Tree

--- BalancedBinaryTree.cpp
+++ BalancedBinaryTree.cpp
@@ -11,13 +11,13 @@ class Solution {
 public:
     int getHeight(TreeNode* root)
     {
-        if (!root)  return 0;
+        if (!root)    return 0;
         return (1 + max(getHeight(root->left), getHeight(root->right)));
     }
     
     bool isBalanced(TreeNode* root) {
-        if (!root)  return true;
-        int leftHeight = getHeight(root->left);
+        if (!root)    return true;
+        int leftHeight  = getHeight(root->left);
         int rightHeight = getHeight(root->right);
         if (abs(leftHeight - rightHeight) <= 1 && isBalanced(root->left) && isBalanced(root->right))    return true;
         return false;
