b3a73c3f8f2a0359530119e2738b5f0d55ce0dc6
Bhavani Subramanian <bhasubr@gmail.com>
2015-09-21 15:28:20 -0700

    Excel Sheet Column Number

--- ExcelSheetColumnNumber.cpp
+++ ExcelSheetColumnNumber.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int titleToNumber(string s) {
-        map<char, int> charNumMap;
-        int i = 1;
-        for (char curChar = 'A'; curChar <= 'Z' && i <= 26; curChar++, i++)
-            charNumMap[curChar] = i;
-        int number = 0;
-        for (int i = s.length() - 1, power = 1; i >= 0; i--, power *= 26)
-            number += power * charNumMap[s[i]];
-        return number;
+       map<char, int> charNumMap;
+       int i = 1;
+       for (char curChar = 'A'; curChar <= 'Z' && i <= 26; curChar++, i++)
+          charNumMap[curChar] = i;
+       int number = 0;
+       for (int i = s.length() - 1, power = 1; i >= 0; i--, power *= 26)
+          number += power * charNumMap[s[i]];
+       return number;
     }
 };
