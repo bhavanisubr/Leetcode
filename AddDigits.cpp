f19b7c34d0f410c50e19203b77c570bc21cf8271
Bhavani Subramanian <bhasubr@gmail.com>
2015-09-21 15:06:41 -0700

    Add Digits

--- AddDigits.cpp
+++ AddDigits.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int addDigits(int num) {
-        /* Trivial solution:
-        int sum = 0;
-	while (num) {
-	    sum += num % 10;
-	    num /= 10;
-	}
-	if (sum >= 10)   return addDigits(sum);
-	return sum; */
+        // Trivial solution:
+        /* int sum = 0;
+	    while (num) {
+	        sum += num % 10;
+	        num /= 10;
+	    }
+	    if (sum >= 10)   return addDigits(sum);
+	    return sum; */
 		
-	// O(1) time solution:
-	return 1 + ((num - 1) % 9);
+	    // O(1) time solution:
+	    return 1 + ((num - 1) % 9);
     }
 };
