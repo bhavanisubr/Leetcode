4348c90b5ec70452261d3cba4fbc0bbdced85cdd
Bhavani Subramanian <bhasubr@gmail.com>
2015-09-21 15:38:44 -0700

    Lowest Common Ancestor of a Binary Search Tree

--- LCABST.cpp
+++ LCABST.cpp
@@ -10,10 +10,10 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (root == NULL)	return NULL;
-	if (p->val == root->val || q->val == root->val)	return root;
-	if (p->val < root->val && q->val < root->val)	return lowestCommonAncestor(root->left, p, q);
-	if (p->val > root->val && q->val > root->val)	return lowestCommonAncestor(root->right, p, q);
-	return root;
+       if (!root)   return NULL;
+       if (p->val == root->val || q->val == root->val)   return root;
+       if (p->val < root->val && q->val < root->val)   return lowestCommonAncestor(root->left, p, q);
+       if (p->val > root->val && q->val > root->val)   return lowestCommonAncestor(root->right, p, q);
+       return root;
     }
 };
