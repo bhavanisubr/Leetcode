4d5ac79cdf68c82e323784eba5db220ea6c5f939
Bhavani Subramanian <bhasubr@gmail.com>
2015-09-21 15:41:23 -0700

    Maximum Depth of Binary Tree

--- MaxDepthOfBinaryTree.cpp
+++ MaxDepthOfBinaryTree.cpp
@@ -10,7 +10,7 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-       if (root == NULL)   return 0;
+       if (!root)   return 0;
        int leftDepth = maxDepth(root->left);
        int rightDepth = maxDepth(root->right);
        return (leftDepth > rightDepth ? leftDepth + 1 : rightDepth + 1);
