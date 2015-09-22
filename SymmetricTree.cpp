08393630d89799dc70aad423950d093d95119a80
Bhavani Subramanian <bhasubr@gmail.com>
2015-09-21 21:47:53 -0700

    Update SymmetricTree.cpp

--- SymmetricTree.cpp
+++ SymmetricTree.cpp
@@ -1,4 +1,21 @@
 /**
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+ * For example, this binary tree is symmetric:
+ 
+        1
+       / \
+      2   2
+     / \ / \
+    3  4 4  3
+    
+    But the following is not:
+    
+        1
+       / \
+      2   2
+       \   \
+       3    3
+ * 
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
@@ -6,7 +23,8 @@
  *     TreeNode *right;
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
- */
+ **/
+ 
 class Solution {
 public:
     bool isSymmetricUtil(TreeNode* root1, TreeNode* root2) {
