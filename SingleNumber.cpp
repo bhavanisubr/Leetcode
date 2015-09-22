c9468910e8a827ebfef0c481982766cad63380ee
Bhavani Subramanian <bhasubr@gmail.com>
2015-09-21 17:23:13 -0700

    Single Number

--- SingleNumber.cpp
+++ SingleNumber.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-	for (auto it = nums.begin(); it != nums.end(); ++it)
-	{
-		if (*it == *(++it))	continue;
-		return *(it - 1);
-	}
+       sort(nums.begin(), nums.end());
+       for (auto it = nums.begin(); it != nums.end(); ++it) {
+          if (*it == *(++it))   continue;
+          return *(it - 1);
+       }
     }
 };
