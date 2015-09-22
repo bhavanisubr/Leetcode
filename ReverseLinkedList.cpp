199e55612a290ec5f48d2338f7212c83c8bf8713
Bhavani Subramanian <bhasubr@gmail.com>
2015-09-21 17:18:55 -0700

    Reverse Linked List

--- ReverseLinkedList.cpp
+++ ReverseLinkedList.cpp
@@ -9,23 +9,22 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        // Iterative solution:
-        /* ListNode* prev = NULL;
-        ListNode* current = head;
-        ListNode* next;
-        while (current != NULL)
-        {
-            next = current->next;
-            current->next = prev;
-            prev = current;
-            current = next;
-        }
-        return prev; */
-
+       /* Iterative solution:
+       ListNode* prev = NULL;
+       ListNode* current = head;
+       ListNode* next;
+       while (current != NULL) {
+          next = current->next;
+          current->next = prev;
+          prev = current;
+          current = next;
+       }
+       return prev; */
+       
         // Recursive solution:
-        if (head == NULL)   return NULL;
+        if (!head)   return NULL;
         ListNode* rest = head->next;
-        if (rest == NULL)   return head;
+        if (!rest)   return head;
         rest = reverseList(rest);
         head->next->next = head;
         head->next = NULL;
