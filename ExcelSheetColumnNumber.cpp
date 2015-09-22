c4bd46c083f4fceca6f4117d68187765093faa97
Bhavani Subramanian <bhasubr@gmail.com>
2015-09-21 21:17:01 -0700

    Update ExcelSheetColumnNumber.cpp

--- ExcelSheetColumnNumber.cpp
+++ ExcelSheetColumnNumber.cpp
@@ -1,3 +1,15 @@
+/**
+ * Given a column title as appears in an Excel sheet, return its corresponding column number.
+ * For example:
+ * A -> 1
+ * B -> 2
+ * C -> 3
+ * ...
+ * Z -> 26
+ * AA -> 27
+ * AB -> 28 
+ **/
+ 
 class Solution {
 public:
     int titleToNumber(string s) {
