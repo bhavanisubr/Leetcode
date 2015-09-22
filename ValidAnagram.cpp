a46d2d18349a7795a3aca3950186a0c58a6df13a
Bhavani Subramanian <bhasubr@gmail.com>
2015-09-21 17:29:34 -0700

    Valid Anagram

--- ValidAnagram.cpp
+++ ValidAnagram.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // Solution 1:
-        /*map<char, int> charCountMap;
-        for (int i = 0; i < s.size(); i++)
-            ++charCountMap[s[i]];
-        for (int i = 0; i < t.size(); i++)
-        {
-            auto it = charCountMap.find(t[i]);
-            if (it->second == 0)    return false;
-            --(it->second);
-        }
-        for (auto it = charCountMap.begin(); it != charCountMap.end(); it++)
-            if (it->second) return false;
-        return true;*/
-        
-        // Solution 2:
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+       /* Solution 1:
+       map<char, int> charCountMap;
+       for (int i = 0; i < s.size(); i++)   ++charCountMap[s[i]];
+       for (int i = 0; i < t.size(); i++) {
+          auto it = charCountMap.find(t[i]);
+          if (it->second == 0)   return false;
+          --(it->second);
+       }
+       for (auto it = charCountMap.begin(); it != charCountMap.end(); it++)
+          if (it->second) return false;
+       return true;*/
+       
+       // Solution 2:
+       sort(s.begin(), s.end());
+       sort(t.begin(), t.end());
+       return s == t;
     }
 };
