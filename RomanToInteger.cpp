0f0bc9c1dab0824d72419a16d445884d76c4bfaf
Bhavani Subramanian <bhasubr@gmail.com>
2015-09-21 17:20:26 -0700

    Roman to Integer

--- RomanToInteger.cpp
+++ RomanToInteger.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    int romanToInt(string s) 
-    {
-        map<char, int> romanToIntMap;
-	romanToIntMap['I'] = 1;
-	romanToIntMap['V'] = 5;
-	romanToIntMap['X'] = 10;
-	romanToIntMap['L'] = 50;
-	romanToIntMap['C'] = 100;
-	romanToIntMap['D'] = 500;
-	romanToIntMap['M'] = 1000;
-
-	int total = romanToIntMap.find(s.back())->second;
-	for (int i = s.length() - 2; i >= 0; i--)
-	{
-		int currentNum = romanToIntMap.find(s[i])->second;
-		int nextNum = romanToIntMap.find(s[i + 1])->second;
-		total = currentNum >= nextNum ? total + currentNum : total - currentNum;
-	}
-	return total;
+    int romanToInt(string s) {
+       map<char, int> romanToIntMap;
+       romanToIntMap['I'] = 1;
+       romanToIntMap['V'] = 5;
+       romanToIntMap['X'] = 10;
+       romanToIntMap['L'] = 50;
+       romanToIntMap['C'] = 100;
+       romanToIntMap['D'] = 500;
+       romanToIntMap['M'] = 1000;
+       
+       int total = romanToIntMap.find(s.back())->second;
+       for (int i = s.length() - 2; i >= 0; i--) {
+          int currentNum = romanToIntMap.find(s[i])->second;
+          int nextNum = romanToIntMap.find(s[i + 1])->second;
+          total = currentNum >= nextNum ? total + currentNum : total - currentNum;
+       }
+       return total;
     }
 };
