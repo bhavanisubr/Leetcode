8bed066729552be0e360b03d40719bb983d08453
bhavanisubr <bhasubr@gmail.com>
2015-11-13 15:23:10 -0800

    Adding new file

---
+++ sum_root_to_leaf_numbers.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+  int sumNumbersUtil(TreeNode* root, int sum) {
+    if (!root->left && !root->right)  return 10 * sum + root->val;
+    int temp = 0;
+    if (root->left)  temp += sumNumbersUtil(root->left, 10 * sum + root->val);
+    if (root->right)  temp += sumNumbersUtil(root->right, 10 * sum + root->val);
+    return temp;
+  }
+
+  int sumNumbers(TreeNode* root) {
+    if (!root)  return 0;
+    return sumNumbersUtil(root, 0);
+  }
+};
