e072e491a27e29b51cafc56fb960d4d06f2c4ff8
bhavanisubr <bhasubr@gmail.com>
2015-11-15 12:25:44 -0800

    Adding new file

--- sum_root_to_leaf_numbers.cpp
+++ sum_root_to_leaf_numbers.cpp
@@ -1,3 +1,17 @@
+/**
+ * Given a binary tree where each node contains digits from 0-9 only and if each root-to-leaf path could be 
+ * represented by a number, i.e. Consider an example where the root-to-leaf path 1->2->3 is represented by 
+ * the number 123, find the total sum of all root-to-leaf numbers.
+ *
+ * For example:
+     1
+    / \
+   2   3
+ * The root-to-leaf path 1->2 represents the number 12.
+ * The root-to-leaf path 1->3 represents the number 13.
+ * Return the sum = 12 + 13 = 25.
+ */
+
 class Solution {
 public:
   int sumNumbersUtil(TreeNode* root, int sum) {
