f51f9d84dd25ae73cc3d30e105b515513190ddb4
bhavanisubr <bhasubr@gmail.com>
2015-11-21 00:19:48 -0800

    Adding new file

--- unique_bsts.cpp
+++ unique_bsts.cpp
@@ -13,20 +13,34 @@
 
 class Solution {
 public:
-  int numTrees(int n) {
-    if (n <= 0)  return INT_MAX;
-    vector<int> results(n + 1, 1); 
-    if (n == 1)  return results[n];
-    for (int idx = 2; idx <= n; idx++) {   
-      int out = 0;
-      for (int i = 0; i < idx; i++) {   
-        int leftRange = 1, rightRange = 1;
-        if (i - 1)  leftRange = results[i];
-        if (i + 1 < idx)  rightRange = results[idx - i - 1]; 
-        out += leftRange * rightRange;
-      }
-      results[idx] = out;
+  long getBinomialCoefficient(int n, int k) {
+    if (k > n - k)  k = n - k;
+    long res = 1;
+    for (int i = 0; i < k; i++) {
+      res *= n - i;
+      res /= i + 1;
     }
-    return results[n];
+    return res;
+  }
+
+  int numTrees(int n) {
+    // NOTE: The number of structurally unique BSTs for a given n corresponds to the nth Catalan
+    // number (a sequence of natural numbers)
+    
+    // Method 1: Dynammic programming - O(n^2) time and O(n) space
+    /*if (n < 0)  return INT_MAX;
+    vector<int> results(n + 1, 0);
+    results[0] = results[1] = 1;
+    if (n <= 1)  return results[n];
+    for (int i = 2; i <= n; i++) {
+      for (int j = 0; j < i; j++)
+        results[i] += results[j] * results[i - j - 1];
+    }
+    return results[n];*/
+
+    // Method 2: Binomial coefficient - O(n) time and O(1) space
+    if (n < 0)  return INT_MAX;
+    if (n <= 1)  return 1;
+    return getBinomialCoefficient(2 * n, n) / (n + 1);
   }
 };
