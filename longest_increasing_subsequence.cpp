57df7f1e05931684d269bbf4f5d3c0b564fee0fa
Bhavani Subramanian <bhasubr@gmail.com>
2015-12-03 10:28:16 -0800

    Update longest_increasing_subsequence.cpp

--- longest_increasing_subsequence.cpp
+++ longest_increasing_subsequence.cpp
@@ -22,7 +22,7 @@ public:
   }
 
   int lengthOfLIS(vector<int>& nums) {
-    // Method 1: Dynammic programming - O(n^2) time
+    // Method 1: Dynamic programming - O(n^2) time
     /*if (nums.empty())  return 0;
     vector<int> maxLISLengthSoFar(nums.size(), 0);
     for (int i = 0; i < nums.size(); i++) {
